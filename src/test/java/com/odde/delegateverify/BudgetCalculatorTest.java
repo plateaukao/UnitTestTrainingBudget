1e9af2e1187f7a6fd00ca9e54d95d5d8c9f4d5ea
Daniel Kao <daniel.kao@linecorp.com>
2018-06-27 17:52:13 +0800

    remove unnecessary function

--- src/test/java/com/odde/delegateverify/BudgetCalculatorTest.java
+++ src/test/java/com/odde/delegateverify/BudgetCalculatorTest.java
@@ -26,45 +26,33 @@ public class BudgetCalculatorTest {
     }
 
     @Test
-    public void invalid_dates() {
-        LocalDate startDate = of(2018, 1,2);
-        LocalDate endDate = of(2018, 1,1);
-
-    }
-    @Test
     public void one_month() {
-        assertQueryEquals(31, of(2018, 1, 31), of(2018, 1, 1)
-        );
+        assertQueryEquals(31, of(2018, 1, 31), of(2018, 1, 1));
     }
 
     @Test
     public void multiple_months() {
-        assertQueryEquals(180, of(2018, 3, 31), of(2018, 1, 1)
-        );
+        assertQueryEquals(180, of(2018, 3, 31), of(2018, 1, 1));
     }
 
     @Test
     public void within_one_month() {
-        assertQueryEquals(22, of(2018, 2, 25), of(2018, 2, 15)
-        );
+        assertQueryEquals(22, of(2018, 2, 25), of(2018, 2, 15));
     }
 
     @Test
     public void across_months() {
-        assertQueryEquals(73, of(2018, 3, 15), of(2018, 2, 15)
-        );
+        assertQueryEquals(73, of(2018, 3, 15), of(2018, 2, 15));
     }
 
     @Test
     public void no_budget_case() {
-        assertQueryEquals(129, of(2018, 4, 20), of(2018, 2, 11)
-        );
+        assertQueryEquals(129, of(2018, 4, 20), of(2018, 2, 11));
     }
 
     @Test
     public void another_no_budget_case() {
-        assertQueryEquals(0, of(2018, 5, 30), of(2018, 5, 1)
-        );
+        assertQueryEquals(0, of(2018, 5, 30), of(2018, 5, 1));
     }
 
     private void assertQueryEquals(int budgetValue, LocalDate endDate, LocalDate startDate) {
